f2eea40a721d44e839af35f0630dd4afb6ed2cbe
FazarBudiman <fazarbudiman1907@gmail.com>
2024-12-06 02:36:03 +0700

    push-development

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,145 +4,49 @@ import { SvgIcon } from "../../common/SvgIcon";
 import Container from "../../common/Container";
 import { RiInstagramFill } from "react-icons/ri";
 
-import i18n from "i18next";
 import {
   FooterSection,
-  Title,
   NavLink,
-  Extra,
   LogoContainer,
   Para,
-  Large,
-  Chat,
-  Empty,
   FooterContainer,
   Language,
-  Label,
-  LanguageSwitch,
-  LanguageSwitchContainer,
 } from "./styles";
 
-interface SocialLinkProps {
-  href: string;
-  src: string;
-}
 
 const Footer = ({ t }: { t: TFunction }) => {
-  const handleChange = (language: string) => {
-    i18n.changeLanguage(language);
-  };
-
-  const SocialLink = ({ href, src }: SocialLinkProps) => {
-    return (
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-        key={src}
-        aria-label={src}
-      >
-        {/* <SvgIcon src={src} width="25px" height="25px" /> */}
-        <RiInstagramFill />
-      </a>
-    );
-  };
 
   return (
     <>
       <FooterSection>
         <Container>
-          {/* <Row justify="space-between">
-            <Col lg={10} md={10} sm={12} xs={12}>
-              <Language>{t("Contact")}</Language>
-              <Large to="/">{t("Tell us everything")}</Large>
-              <Para>
-                {t(`Do you have any question? Feel free to reach out.`)}
-              </Para>
-              <a href="mailto:l.qqbadze@gmail.com">
-                <Chat>{t(`Let's Chat`)}</Chat>
-              </a>
+          <Row justify="space-between" gutter={[0, 14]}>
+          <Col lg={1} md={1} sm={{order:1, span:1}} xs={{order:2, span:4}}>
+              <NavLink to="/">
+                <LogoContainer>
+                  <SvgIcon
+                    src="logo.svg"
+                    aria-label="homepage"
+                    width="101px"
+                    height="64px"
+                  />
+                </LogoContainer>
+              </NavLink>
             </Col>
-            <Col lg={8} md={8} sm={12} xs={12}>
-              <Title>{t("Policy")}</Title>
-              <Large to="/">{t("Application Security")}</Large>
-              <Large to="/">{t("Software Principles")}</Large>
-            </Col>
-            <Col lg={6} md={6} sm={12} xs={12}>
-              <Empty />
-              <Large to="/">{t("Support Center")}</Large>
-              <Large to="/">{t("Customer Support")}</Large>
-            </Col>
-          </Row> */}
-          <Row justify="space-between"  gutter={[12, 12]}>
-            <Col lg={14} md={14} sm={24} xs={24}>
+            <Col lg={16} md={16} sm={12} xs={{order:1, span:24}}>
               <Language>{t("Address")}</Language>
-              <Para>Rancho Santa Margarita</Para>
-              <Para>2131 Elk Street</Para>
-              <Para>California</Para>
+              <Para>Jl. Juraeji No 02 Kampung Mekarsari</Para>
+              <Para>Kelurahan Palasari Kecamatan Cibiru</Para>
+              <Para>Kota Bandung</Para>
             </Col>
-            {/* <Col lg={10} md={10} sm={24} xs={24}>
-              <Title>{t("Company")}</Title>
-              <Large to="/">{t("About")}</Large>
-              <Large to="/">{t("Blog")}</Large>
-              <Large to="/">{t("Press")}</Large>
-              <Large to="/">{t("Careers & Culture")}</Large>
-            </Col> */}
-            {/* <Col lg={6} md={6} sm={12} xs={12}>
-              <Label htmlFor="select-lang">{t("Language")}</Label>
-              <LanguageSwitchContainer>
-                <LanguageSwitch onClick={() => handleChange("en")}>
-                  <SvgIcon
-                    src="united-states.svg"
-                    aria-label="homepage"
-                    width="30px"
-                    height="30px"
-                  />
-                </LanguageSwitch>
-                <LanguageSwitch onClick={() => handleChange("es")}>
-                  <SvgIcon
-                    src="spain.svg"
-                    aria-label="homepage"
-                    width="30px"
-                    height="30px"
-                  />
-                </LanguageSwitch>
-              </LanguageSwitchContainer>
-            </Col> */}
+            <Col lg={2} md={2} sm={2} xs={{order:3, span:4}}>
+              <FooterContainer>
+                <a href="https://www.instagram.com/prekostion" target="_blank" rel="noopener noreferrer"><RiInstagramFill color="#FF9800" size={"3em"} /></a>
+              </FooterContainer>
+            </Col>
           </Row>
         </Container>
       </FooterSection>
-      <Extra>
-        <Container border={true}>
-          <Row
-            justify="space-between"
-            align="middle"
-            style={{ paddingTop: "3rem" }}
-          >
-            <Col lg={22} md={22} sm={22} xs={22}>
-            <NavLink to="/">
-              <LogoContainer>
-                <SvgIcon
-                  src="logo.svg"
-                  aria-label="homepage"
-                  width="101px"
-                  height="64px"
-                />
-              </LogoContainer>
-            </NavLink>
-            </Col>
-            <Col lg={2} md={2} sm={2} xs={2}>
-            <FooterContainer>
-              {/* <SocialLink
-                href="https://github.com/Adrinlol/create-react-app-adrinlol"
-                src="github.svg"
-              /> */}
-              <a href="https://www.instagram.com/prekostion" target="_blank" rel="noopener noreferrer"><RiInstagramFill color="#FF9800" size={"3em"} /></a>
-            </FooterContainer>
-            </Col>
-            
-          </Row>
-        </Container>
-      </Extra>
     </>
   );
 };
